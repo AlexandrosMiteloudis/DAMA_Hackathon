08d521f25a6c65cae9cfd0ad6da2c37ac932dcd2
terry07 <07stamatis07@gmail.com>
2026-04-03 22:16:00 +0300

    fix: add 'load_metabric_data' from other branch

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+import pandas as pd
+import kagglehub
+import os
+
+def load_metabric_data():
+    print("Fetching METABRIC dataset from Kaggle...")
+    dataset_dir = kagglehub.dataset_download("raghadalharbi/breast-cancer-gene-expression-profiles-metabric")
+    
+    csv_path = os.path.join(dataset_dir, "METABRIC_RNA_Mutation.csv")
+    
+    df = pd.read_csv(csv_path, low_memory=False)
+    
+    return df
+
 def count_feature_categories(feature_columns):
     clinical_cols = []
     mutation_cols = []
